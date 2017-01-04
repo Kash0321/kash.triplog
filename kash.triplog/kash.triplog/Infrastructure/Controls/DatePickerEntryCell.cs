34715d8d90888e625b3d7bae1ab7aeee392ded0a
Kash0321 <castillo.sergio@gmail.com>
2017-01-04 16:26:14 +0100

    Selector de fecha (DatePicker)

---
+++ kash.triplog/kash.triplog/Infrastructure/Controls/DatePickerEntryCell.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace kash.triplog.Infrastructure.Controls
+{
+    public class DatePickerEntryCell : EntryCell
+    {
+        public static readonly BindableProperty DateProperty =
+            BindableProperty.Create<DatePickerEntryCell, DateTime>(p =>
+                                p.Date,
+                                DateTime.Now,
+                                propertyChanged: new BindableProperty.BindingPropertyChangedDelegate<DateTime>(DatePropertyChanged));
+
+        public DateTime Date
+        {
+            get { return (DateTime)GetValue(DateProperty); }
+            set { SetValue(DateProperty, value); }
+        }
+
+        public new event EventHandler Completed;
+
+        static void DatePropertyChanged(BindableObject bindable, DateTime oldValue, DateTime newValue)
+        {
+            var @this = (DatePickerEntryCell)bindable;
+            @this.Completed?.Invoke(bindable, new EventArgs());
+        }
+    }
+}
